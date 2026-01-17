59c0bf8767d97f8dfd198cfaf2c4951feff783f1
jared zhang <jared.zhang@emergenttech.com>
2026-01-17 18:32:31 +0800

    deprecated UT

--- tests/my_test.py
+++ tests/my_test.py
@@ -1,24 +1,24 @@
-from langgraph_sdk import get_sync_client
-
-# 调用Agent发布的API接口
-client = get_sync_client(url="http://localhost:2024")
-
-for chunk in client.runs.stream(
-        None,
-        "agent",
-        input={
-            "messages": [{
-                "role": "human",
-                "content": "当前用户的名字是什么？"
-            }],
-        },
-        stream_mode="messages",
-        config={"configurable": {'username': 'jared'}}
-):
-    # print(f"Receiving new event of type: {chunk.event}...")
-    # print(chunk.data)
-    # print("\n\n")
-    if isinstance(chunk.data, list) and 'type' in chunk.data[0] and chunk.data[0]['type'] == 'ai':
-        print(chunk.data[0]['content'], end='|')
-# if __name__ == "__main__":
-#     run(main())
+# from langgraph_sdk import get_sync_client
+#
+# # 调用Agent发布的API接口
+# client = get_sync_client(url="http://localhost:2024")
+#
+# for chunk in client.runs.stream(
+#         None,
+#         "agent",
+#         input={
+#             "messages": [{
+#                 "role": "human",
+#                 "content": "当前用户的名字是什么？"
+#             }],
+#         },
+#         stream_mode="messages",
+#         config={"configurable": {'username': 'jared'}}
+# ):
+#     # print(f"Receiving new event of type: {chunk.event}...")
+#     # print(chunk.data)
+#     # print("\n\n")
+#     if isinstance(chunk.data, list) and 'type' in chunk.data[0] and chunk.data[0]['type'] == 'ai':
+#         print(chunk.data[0]['content'], end='|')
+# # if __name__ == "__main__":
+# #     run(main())
--- tests/my_test_async.py
+++ tests/my_test_async.py
@@ -1,25 +1,25 @@
-from langgraph_sdk import get_client
-import asyncio
-# 调用Agent发布的API接口
-client = get_client(url = "http://localhost:2024")
-
-async def main():
-    async for chunk in client.runs.stream(
-        None,
-        "agent",
-        input={
-            "messages":[{
-                "role": "human",
-                "content": "给当前用户一个祝福语"
-            }],
-        },
-        config={"configurable": {'username': 'jared'}}
-    ):
-        print(f"Receiving new event of type: {chunk.event}...")
-        print(chunk.data)
-        print("\n\n")
-        if isinstance(chunk.data, list) and 'type' in chunk.data[0] and chunk.data[0]['type'] == 'ai':
-            print(chunk.data[0]['content'], end='|')
-
-if __name__ == "__main__":
-    asyncio.run(main())
+# from langgraph_sdk import get_client
+# import asyncio
+# # 调用Agent发布的API接口
+# client = get_client(url = "http://localhost:2024")
+#
+# async def main():
+#     async for chunk in client.runs.stream(
+#         None,
+#         "agent",
+#         input={
+#             "messages":[{
+#                 "role": "human",
+#                 "content": "给当前用户一个祝福语"
+#             }],
+#         },
+#         config={"configurable": {'username': 'jared'}}
+#     ):
+#         print(f"Receiving new event of type: {chunk.event}...")
+#         print(chunk.data)
+#         print("\n\n")
+#         if isinstance(chunk.data, list) and 'type' in chunk.data[0] and chunk.data[0]['type'] == 'ai':
+#             print(chunk.data[0]['content'], end='|')
+#
+# if __name__ == "__main__":
+#     asyncio.run(main())
